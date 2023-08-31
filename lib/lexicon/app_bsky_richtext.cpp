7bc28354e2e8130e4da01a24774192056457083d
Michel de Boer <michelboer@xs4all.nl>
2023-08-31 13:08:12 +0200

    Function to apply facets

--- lib/lexicon/app_bsky_richtext.cpp
+++ lib/lexicon/app_bsky_richtext.cpp
@@ -78,4 +78,103 @@ Facet::Ptr Facet::fromJson(const QJsonObject& json)
     return facet;
 }
 
+struct HyperLink
+{
+    int mStart;
+    int mEnd;
+    QString mText;
+};
+
+static QString createHtmlLink(const QString& linkText, const Facet::Feature& feature)
+{
+    switch (feature.mType)
+    {
+    case ATProto::AppBskyRichtext::Facet::Feature::Type::MENTION:
+    {
+        const auto& facetMention = std::get<ATProto::AppBskyRichtext::FacetMention::Ptr>(feature.mFeature);
+        return QString("<a href=\"%1\">%2</a>").arg(facetMention->mDid, linkText);
+        break;
+    }
+    case ATProto::AppBskyRichtext::Facet::Feature::Type::LINK:
+    {
+        const auto& facetLink = std::get<ATProto::AppBskyRichtext::FacetLink::Ptr>(feature.mFeature);
+        return QString("<a href=\"%1\">%2</a>").arg(facetLink->mUri, linkText);
+        break;
+    }
+    case ATProto::AppBskyRichtext::Facet::Feature::Type::UNKNOWN:
+        qWarning() << "Uknown facet type:" << int(feature.mType) << "link:" << linkText;
+        break;
+    }
+
+    return {};
+}
+
+static std::map<int, HyperLink> buildStartLinkMap(const QByteArray& bytes, const std::vector<AppBskyRichtext::Facet::Ptr>& facets)
+{
+    std::map<int, HyperLink> startLinkMap;
+
+    for (const auto& facet : facets)
+    {
+        if (facet->mFeatures.empty())
+        {
+            qWarning() << "Empty facet:" << QString(bytes);
+            continue;
+        }
+
+        // What to do with multiple features?
+        if (facet->mFeatures.size() > 1)
+            qWarning() << "Multiple features, taking only the first";
+
+        HyperLink link;
+        link.mStart = facet->mIndex.mByteStart;
+        link.mEnd = facet->mIndex.mByteEnd;
+        const int sliceSize = link.mEnd - link.mStart;
+
+        if (link.mStart < 0 || link.mEnd > bytes.size() || sliceSize < 0)
+        {
+            qWarning() << "Invalid index in facet:" << QString(bytes);
+            continue;
+        }
+
+        const auto linkText = QString(bytes.sliced(link.mStart, sliceSize));
+        const auto& feature = facet->mFeatures.front();
+        link.mText = createHtmlLink(linkText, feature);
+
+        if (!link.mText.isEmpty())
+            startLinkMap[link.mStart] = link;
+    }
+
+    return startLinkMap;
+}
+
+QString applyFacets(const QString& text, const std::vector<AppBskyRichtext::Facet::Ptr>& facets)
+{
+    const auto& bytes = text.toUtf8();
+    const auto startLinkMap = buildStartLinkMap(bytes, facets);
+
+    QString result;
+    int bytePos = 0;
+
+    for (const auto& [start, link] : startLinkMap)
+    {
+        if (start < bytePos)
+        {
+            qWarning() << "Overlapping facets:" << text;
+            result.clear();
+            bytePos = 0;
+            break;
+        }
+
+        const auto before = bytes.sliced(bytePos, start - bytePos);
+        result.append(QString(before).toHtmlEscaped());
+        result.append(link.mText);
+        bytePos = link.mEnd;
+    }
+
+    result.append(QString(bytes.sliced(bytePos)).toHtmlEscaped());
+    qDebug() << "Orig:   " << text;
+    qDebug() << "Faceted:" << result;
+    return result;
+}
+
 }
