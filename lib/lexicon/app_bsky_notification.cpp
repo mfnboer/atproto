ee0a2ce41b761fa4affe6ed3d65dd7ef7aa34925
Michel de Boer <michelboer@xs4all.nl>
2023-11-06 16:10:40 +0100

    Invite code notifications

--- lib/lexicon/app_bsky_notification.cpp
+++ lib/lexicon/app_bsky_notification.cpp
@@ -55,6 +55,7 @@ Notification::Ptr Notification::fromJson(const QJsonObject& json)
         case NotificationReason::QUOTE:
             notification->mRecord = AppBskyFeed::Record::Post::fromJson(recordJson);
             break;
+        case NotificationReason::INVITE_CODE_USED:
         case NotificationReason::UNKNOWN:
             qWarning() << "Unknow notification reason:" << notification->mRawReason;
             break;
