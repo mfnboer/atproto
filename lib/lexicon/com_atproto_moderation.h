5db187b537cd327e84d507fc354a4f2bae7db41f
Michel de Boer <michelboer@xs4all.nl>
2023-11-24 20:08:53 +0100

    Moderation report

--- lib/lexicon/com_atproto_moderation.h
+++ lib/lexicon/com_atproto_moderation.h
@@ -16,6 +16,7 @@ enum class ReasonType
 };
 
 QString reasonTypeToString(ReasonType reason);
-QString reasonToDescription(ReasonType reason);
+QString reasonTypeToDescription(ReasonType reason);
+QString reasonTypeToTitle(ReasonType reason);
 
 }
