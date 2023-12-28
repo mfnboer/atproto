90e58730ba373b1613fc9cebad3209a4b3644e55
Michel de Boer <michelboer@xs4all.nl>
2023-12-28 20:33:42 +0100

    Allow colon in rkey

--- lib/at_regex.cpp
+++ lib/at_regex.cpp
@@ -4,8 +4,8 @@
 
 namespace ATProto {
 
-const QRegularExpression ATRegex::HANDLE{ R"(([a-zA-Z0-9]([a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z]([a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)" };
-const QRegularExpression ATRegex::RKEY{ R"([a-zA-Z0-9\.\-_~]{1,512})" };
-const QRegularExpression ATRegex::DID{ R"(did:[a-z]+:[a-zA-Z0-9-\.:_]+)"};
+const QRegularExpression ATRegex::HANDLE{ R"(([a-zA-Z0-9]([a-zA-Z0-9\-]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z]([a-zA-Z0-9\-]{0,61}[a-zA-Z0-9])?)" };
+const QRegularExpression ATRegex::RKEY{ R"([a-zA-Z0-9\.\-_~:]{1,512})" };
+const QRegularExpression ATRegex::DID{ R"(did:[a-z]+:[a-zA-Z0-9\-\.:_]+)"};
 
 }
