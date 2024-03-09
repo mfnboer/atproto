8d6599a9ff5e3a62444bd2b5176e77871e4a0ce4
Michel de Boer <michelboer@xs4all.nl>
2024-03-09 16:16:18 +0100

    applyWrites

--- lib/lexicon/com_atproto_repo.h
+++ lib/lexicon/com_atproto_repo.h
@@ -51,4 +51,42 @@ struct ListRecordsOutput
     static Ptr fromJson(const QJsonObject& json);
 };
 
+// com.atproto.repo.applyWrites#create
+struct ApplyWritesCreate
+{
+    QString mCollection;
+    std::optional<QString> mRKey;
+    QJsonObject mValue;
+
+    QJsonObject toJson() const;
+
+    using Ptr = std::unique_ptr<ApplyWritesCreate>;
+};
+
+// com.atproto.repo.applyWrites.defs#update
+struct ApplyWritesUpdate
+{
+    QString mCollection;
+    QString mRKey;
+    QJsonObject mValue;
+
+    QJsonObject toJson() const;
+
+    using Ptr = std::unique_ptr<ApplyWritesUpdate>;
+};
+
+// com.atproto.repo.applyWrites.defs#delete
+struct ApplyWritesDelete
+{
+    QString mCollection;
+    QString mRKey;
+
+    QJsonObject toJson() const;
+
+    using Ptr = std::unique_ptr<ApplyWritesDelete>;
+};
+
+using ApplyWritesType = std::variant<ApplyWritesCreate::Ptr, ApplyWritesUpdate::Ptr, ApplyWritesDelete::Ptr>;
+using ApplyWritesList = std::vector<ApplyWritesType>;
+
 }
